cee9686cd7baa6ee02b757cb9ed63135ec07328a
Chris Mems <christopher.mems@.com>
2023-03-04 10:48:43 -0600

    set up intial view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border-2 border-black m-0">
+      <div className="grid grid-cols-3">
+        <div className="border-2 border-black m-0">
+          <h2 className="border-4 border-b-[#23c429]">In Progress</h2>
+          <ul>
+            <li>
+              123
+            </li>
+            <li>
+              456
+            </li>
+            <li>
+              789
+            </li>
+            <li>
+              1239
+            </li>
+            <li>
+              16456
+            </li>
+          </ul>
+        </div>
+        <div className="border-2 border-black m-0">
+          <h2 className="border-4 border-b-[#23c4a9]">Now Serving</h2>
+          <ul>
+            <li>
+              123
+            </li>
+            <li>
+              456
+            </li>
+            <li>
+              789
+            </li>
+            <li>
+              1239
+            </li>
+            <li>
+              16456
+            </li>
+          </ul>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
